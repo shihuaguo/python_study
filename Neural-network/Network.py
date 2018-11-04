08c68342cf8e701d4f1cda874e10677b484b59ce
shihuaguo <147402691@qq.com>
2018-11-04 18:26:32 +0800

    add udpate_mini_batch method

--- Neural-network/Network.py
+++ Neural-network/Network.py
@@ -1,4 +1,9 @@
 import numpy as np
+import random
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
 
 
 class Network(object):
@@ -9,23 +14,32 @@ class Network(object):
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
-    def sigmoid(z):
-        return 1.0/(1.0+np.exp(-z))
-
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
-            a = self.sigmoid(np.dot(w, a) + b)
+            a = sigmoid(np.dot(w, a) + b)
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batchs = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batchs:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {0} complete".format(j))
+
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
 
 
-a = [2, 3, 1]
-net = Network(a)
-print(net.biases)
-print(net.weights)
-
-b = zip(a[:-1], a[1:])
-print('b={}'.format(b))
-for x, y in b:
-    print('x={}, y={}'.format(x, y))
-
-for x, y in zip(a[:-1], a[1:]):
-    print('x={}, y={}'.format(x, y))
